f9472c45652720b477635038c30b0b293edfdc26
sheng <2094038054@qq.com>
2023-02-24 20:46:10 +0800

    The fifth commit

--- src/main/java/cn/edu/hactcm/main/GitTest.java
+++ src/main/java/cn/edu/hactcm/main/GitTest.java
@@ -2,10 +2,6 @@ package cn.edu.hactcm.main;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello,Git!");
-        System.out.println("hello word");
-        System.out.println("hello word");
-        System.out.println("hello word");
         System.out.println("master test");
         System.out.println("hot-fix test");
 
