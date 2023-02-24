5fe80aa601b4a972e2def53e23c7b13ec7a033e4
sheng <2094038054@qq.com>
2023-02-24 20:23:40 +0800

    The forth commit

--- src/main/java/cn/edu/hactcm/main/GitTest.java
+++ src/main/java/cn/edu/hactcm/main/GitTest.java
@@ -6,5 +6,7 @@ public class GitTest {
         System.out.println("hello word");
         System.out.println("hello word");
         System.out.println("hello word");
+        System.out.println("hello git");
+
     }
 }
