e768845f46692053e6b44d35cfeede74e1fc6e7d
CV-siege-lion <126044737+CV-siege-lion@users.noreply.github.com>
2023-02-24 20:49:04 +0800

    Update GitTest.java

--- src/main/java/cn/edu/hactcm/main/GitTest.java
+++ src/main/java/cn/edu/hactcm/main/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("master test");
         System.out.println("hot-fix test");
-
+        System.out.println("hello git!");
     }
 }
